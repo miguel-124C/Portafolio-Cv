bede7f38768691fdde005564711009198ecae95c
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-06-02 16:18:47 -0400

    Cambia orden de proyectos de mayor-menor import..

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,53 +1,53 @@
 let proyects = [
     {
         img:"img/1.png",
-        dataInfo:`Hecho con la API de "openweathermap", fue la API que conoci y tome en practica, tiene un diseño minimalista`,
-        href:"https://miguel-124c.github.io/datosMeteorologicas/"
-    },
-    {
-        img:"img/2.png",
-        dataInfo:`Utilizando el objeto Date cree mi propio calendario con todos los meses del año actual`,
-        href:"https://miguel-124c.github.io/Calendario-Object-Date-/"
-    },
-    {
-        img:"img/3.png",
         dataInfo:`Copiando las funcionalidades de un reloj de celular. Usando el objeto Date() para el reloj y las funciones setInterval para el cronometro.`,
         href:"https://miguel-124c.github.io/cronometro/"
     },
     {
-        img:"img/4.png",
-        dataInfo:`Un reto por el programa de Alura Latam fue el Encriptador, recibi un certificado por haber concluido el challenge.
-        El diseño se nos fue dado por figma, aprendiendo algo sobre aquel programa.`,
-        href:"https://miguel-124c.github.io/Encriptador-Alura/"
+        img:"img/2.png",
+        dataInfo:`Reloj que cambia en un cierto tiempo de color, pasando por casi todos, tiene la hora exacta y un buen diseño`,
+        href:"https://miguel-124c.github.io/Reloj/"
     },
     {
-        img:"img/5.png",
-        dataInfo:`Un juego más a la lista que tengo, me gusta este ya que me hizo analizar más a fondo para poder hacerlo`,
+        img:"img/3.png",
+        dataInfo:`Un juego que me hacía ilución crear, me gusta el resultado ya que me hizo analizar más a fondo para como poder hacerlo`,
         href:"https://miguel-124c.github.io/Memoriza-la-cancion/"
     },
     {
-        img:"img/6.png",
+        img:"img/4.png",
+        dataInfo:`Otro juego a la lista, me costó bastante ver como hacerlo, más puede terminarlo, futuras mejoras`,
+        href:"https://miguel-124c.github.io/Sudoku/"
+    },
+    {
+        img:"img/5.png",
         dataInfo:`Queriendo aprender más sobre las APIS hice este proyecto. Es un buscador de peliculas que muestra todos sus datos`,
         href:"https://miguel-124c.github.io/Info-Movies/"
     },
     {
+        img:"img/6.png",
+        dataInfo:`Implementando el CRUD hice una lista de tareas. En un futuro cuando sepa más sobre Bases de datos lo modificare`,
+        href:"https://miguel-124c.github.io/CRUD-Lista-de-tareas/"
+    },
+    {
         img:"img/7.png",
-        dataInfo:`En los cursos del programa de Alura Latam aprendi sobre la API de Canvas, con ella hice siguiendo las enseñansas del maestro una pizarra para dibujar, que cambia el color, teniendo los 3 colores RGB.`,
-        href:"https://miguel-124c.github.io/pizarra-canvas/"
+        dataInfo:`Utilizando el objeto Date cree mi propio calendario con todos los meses del año actual`,
+        href:"https://miguel-124c.github.io/Calendario-Object-Date-/"
     },
     {
         img:"img/8.png",
-        dataInfo:`Implementando el CRUD hice una lista de tareas. En un futuro cuando sepa más sobre Bases de datos lo modificare`,
-        href:"https://miguel-124c.github.io/CRUD-Lista-de-tareas/"
+        dataInfo:`Un reto por el programa de Alura Latam fue el Encriptador, recibi un certificado por haber concluido el challenge.
+        El diseño se nos fue dado por figma, aprendiendo algo sobre aquel programa.`,
+        href:"https://miguel-124c.github.io/Encriptador-Alura/"
+    },
+    {   
+        img:"img/9.png",
+        dataInfo:`Hecho con la API de "openweathermap", fue la API que conoci y tome en practica, tiene un diseño minimalista`,
+        href:"https://miguel-124c.github.io/datosMeteorologicas/"
+    },
+    {
+        img:"img/10.png",
+        dataInfo:`En los cursos del programa de Alura Latam aprendi sobre la API de Canvas, con ella hice siguiendo las enseñansas del maestro una pizarra para dibujar, que cambia el color, teniendo los 3 colores RGB.`,
+        href:"https://miguel-124c.github.io/pizarra-canvas/"
     }
-    // {
-    //     img:"img/9.png",
-    //     dataInfo:`Hecho con la API de "openweathermap", fue la API que conoci y tome en practica, tiene un diseño minimalista`,
-    //     href:"https://miguel-124c.github.io/datosMeteorologicas/"
-    // },
-    // {
-    //     img:"img/10.png",
-    //     dataInfo:`Hecho con la API de "openweathermap", fue la API que conoci y tome en practica, tiene un diseño minimalista`,
-    //     href:"https://miguel-124c.github.io/datosMeteorologicas/"
-    // }
 ];
